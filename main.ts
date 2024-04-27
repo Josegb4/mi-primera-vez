6466b1103e19d606ef6fa1b564f7abb6a57806c6
Josegb4 <168300644+Josegb4@users.noreply.github.com>
2024-04-27 13:31:11 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+let ax = 0
+basic.forever(function () {
+    ax = input.acceleration(Dimension.X)
+    if (ax > 818 && ax <= -1023) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > 409 && ax <= 818) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > -409 && ax <= 409) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > -818 && ax <= -409) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (ax > -818 && ax >= -1023) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
